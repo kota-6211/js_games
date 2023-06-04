35d24ab68088994ec3b441766f9d34d79a94688d
kota-6211 <kota_1126@icloud.com>
2023-06-04 14:04:05 +0000

    todo error

--- src/js/modules/todo.js
+++ src/js/modules/todo.js
@@ -1,5 +1,7 @@
 const addForm = document.querySelector('.td-add-form');
 const addInput = document.querySelector('.td-add-input');
+const todosUl = document.querySelector('.todos');
+const donesUl = document.querySelector('.dones');
 
 let todoData = [];
 
@@ -25,3 +27,39 @@ function getTodoData() {
     return JSON.parse(localStorage.getItem('myTodo'));
 }
 
+function createTodoElement(todo) {
+    const todoItem = document.createElement('li');
+    todoItem.classList.add('td-item');
+    const todoContent = document.createElement('p');
+    todoContent.classList.add('td-content');
+    todoContent.textContent = todo.content;
+    todoItem.appendChild(todoContent);
+
+    const btnContainer = document.createElement('div');
+    btnContainer.classList.add('td-btn-container');
+    const btn = document.createElement('img');
+    btn.classList.add('td-btn');
+    const upBtn = btn.cloneNode(false);
+    upBtn.setAttribute('src','./images/todo_button/up.png');
+
+    if (!todo.isDone) {
+        upBtn.classList.add('edit-btn');
+        btn.classList.add('isDone-btn');
+        btn.setAttribute('src','./images/todo_button/ok.png')
+        btnContainer.appendChild(btn);
+        btnContainer.appendChild(upBtn);
+        todoItem.appendChild(btnContainer);
+        todosUl.appendChild(btnContainer);
+    }else{
+        upBtn.classList.add('undo-btn');
+        btn.classList.add('delet-btn');
+        btn.setAttribute('src','./images/todo_button/cancel.png')
+        btnContainer.appendChild(btn);
+        btnContainer.appendChild(upBtn);
+        todoItem.appendChild(btnContainer);
+        donesUl.appendChild(todoItem);
+    }
+}
+
+createTodoElement({content:'remains to do', isDone:false})
+createTodoElement({content:'alredy done', isDone:true})
